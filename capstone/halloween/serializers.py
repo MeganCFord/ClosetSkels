51f319b389748bbf51773e4b99ac83de1ec20a41
Megan Ford <meg.c.ford@gmail.com>
2016-09-18 01:42:00 -0500

    wrote and commented out a bunch of nested serializer stuff. removed id and url from user serializer.

--- capstone/halloween/serializers.py
+++ capstone/halloween/serializers.py
@@ -8,18 +8,73 @@ class ElementSerializer(serializers.HyperlinkedModelSerializer):
     model = Element
     fields = ("id", "name")
 
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+   class Meta: 
+    model = Tag
+    fields = ("id", "name")
+
+class CostumeElementSerializer(serializers.HyperlinkedModelSerializer):
+  tags = TagSerializer(many=True)
+  class Meta:
+    model = CostumeElement
+    fields = ("id", "url", "costume", "element", "description", 'tags')
+
+    # def create(self, data):
+    #   if data["tags"]:
+    #     tags_data = data.pop('tags')
+    #     for tag_data in tags_data:
+    #       tag = Tag.objects.get(tag_data["id"])
+
 class CostumeSerializer(serializers.HyperlinkedModelSerializer):
+  tags = TagSerializer(many=True)
+  costume_elements=CostumeElementSerializer(many=True)
   class Meta: 
     model = Costume
     fields = ("id", 'url', 'owner', 'name', 'description', 'datecreated', 'public', 'costume_elements', 'tags')
 
+    # ALL THIS IS FOR IF I DECIDE I WANT TO CREATE ALL THE STUFF AT ONCE which I am not going to do right now.
+  # def create(self, data):
+  #   tags_data = data.pop('tags')
+  #   costume_elements_data = data.pop("costume_elements")
+  #   for tag_data in tags_data:
+  #         #if the tag already exists, just add it to the costume. if it doesn't exist, create it.
+  #       Tag.objects.create(costumes.add(costume), **tag_data)
+  #   for costume_element_data in costume_elements_data:
+  #         #if the costume element already exists, just add it to the costume. if it doesn't exist, create it.
+  #       ce_tags_data = costume_element_data.pop("tags")
+  #       for ce_tag_data in ce_tags_data:
+  #         #if the tag already exists, just add it to the costume. if it doesn't exist, create it.
+  #         Tag.objects.create(costume_element.add(costume_element_data), **tag_data)
+  #       CostumeElement.objects.create(costumes.add(costume), **costume_element_data)
+  #   costume = Costume.objects.create(**data)
+  #   return costume
+
+  # def update(self, instance, data):
+  #   # Update the costume instance
+  #   if data["name"]:
+  #     instance.name = data['name']
+  #   if data["description"]:
+  #     instance.description = data["description"]
+  #   if data["public"]:
+  #     instance.public = data["public"]
+  #   instance.save()
+
+    # Create or update tag and costume element instances that are in the request
+    # if data["tags"]:
+    #   for tag in data["tags"]:
+    #     # delete any instance tags that aren't in data tags
+    #     # create any instance tags that are in data tags
+    #     # EXAMPLE: page = Page(id=item['page_id'], text=item['text'], book=instance)
+    #     page.save()
+
+    # return instance
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
   costumes = CostumeSerializer(many=True)
   
   class Meta:
     model = User
-    fields = ('id', 'url', 'username', 'costumes')
+    fields = ('username', 'costumes')
 
 
 class BooSerializer(serializers.HyperlinkedModelSerializer):
@@ -28,19 +83,8 @@ class BooSerializer(serializers.HyperlinkedModelSerializer):
   
   class Meta: 
     model = Boo
-    fields = ("id", "url", "date", "user", "costume")
+    fields = ("user", "costume")
 
 
-class CostumeElementSerializer(serializers.HyperlinkedModelSerializer):
-  costume = CostumeSerializer()
-  class Meta:
-    model = CostumeElement
-    fields = ("id", "url", "costume", "element", "description", 'tags')
 
 
-class TagSerializer(serializers.HyperlinkedModelSerializer):
-  costumes = CostumeSerializer(many=True)
-  costumeelements = CostumeElementSerializer(many=True)
-  class Meta: 
-    model = Tag
-    fields = ("id", "url", "name", "costumes", "costumeelements")
