c490a25dad5792e6b217d75f35cf3991f6bc51a1
Megan Ford <meg.c.ford@gmail.com>
2016-09-18 12:41:04 -0500

    can now create tag and element.

--- client/controllers/createCtrl.js
+++ client/controllers/createCtrl.js
@@ -1,21 +1,51 @@
 app.controller("Create",[
   "$scope",
-  function($scope) {
+  "APIFactory",
+  "$timeout",
+  function($scope, APIFactory, $timeout) {
     const create = this;
     create.title="create page";
     
     $scope.$on("username", function(event, data) {
       create.username = data;
     });
+    create.tags = [];
+    create.elements = [];
+
+    create.tag = {"name": "", "costumes": [], "costumeelements": []};
+    create.element = {"name": ""};
+
+    APIFactory.getTags().then((res)=> {create.tags = res; console.log("initial tags", create.tags); $timeout();}, e => console.error);
+    APIFactory.getElements().then((res)=> {create.elements = res; console.log("initial elements", create.elements); $timeout();}, e => console.error);
 
     create.costume = {
       "name": "",
       "description": "", 
-      "datecreated": "", 
+      "datecreated": Date.now(), 
       "public": false, 
       "owner": create.username, 
       "tags": [],
       "costumeelements": []
+    };
+
+    create.createTag = () => {
+      APIFactory.createTag(create.tag).then(() => {
+        APIFactory.getTags().then((res)=> {
+          create.tags = res;
+          console.log(create.tags, "updated tags");
+          $timeout();
+        });
+      });
+    };
+
+    create.createElement = () => {
+      APIFactory.createElement(create.element).then(() => {
+        APIFactory.getElements().then((res)=> {
+          create.elements = res;
+          console.log(create.elements, "updated elements");
+          $timeout();
+        });
+      });
     };
 
   }]);
--- client/factories/APIFactory.js
+++ client/factories/APIFactory.js
@@ -14,10 +14,10 @@ app.factory( "APIFactory", [
         return getApiRoot()
         .then((root) => {
           return $http.get(`${root.costumes}`);
-        }, (e) => console.log(e))
+        }, e => console.error)
         .then((res) => {
           return res.data;
-        }, (e) => console.log(e));
+        }, e => console.error);
       }, 
       getUserCostumes: (username) => {
         return getApiRoot()
@@ -26,9 +26,44 @@ app.factory( "APIFactory", [
           }, (e) => console.log(e))
           .then((res) => {
             // returns in a list since queries return filtered lists. in this case, there will be just one match since Django requires unique usernames in its User model.
-            console.log("should have user costumes here", res.data[0].costumes);
             return res.data[0].costumes;
-          }, (e) => console.log(e));
+          }, e => console.error);
+      }, 
+      getTags: () => {
+        return getApiRoot()
+        .then((root) => {
+          return $http.get(`${root.tags}`);
+        }, e => console.error)
+        .then((res)=> {
+          return res.data;
+        }, e => console.error);
+      },
+      createTag: (data) => {
+        return getApiRoot()
+          .then((root) => {
+            return $http.post(`${root.tags}`, data);
+          }, e => console.error);
+          // .then((res) => {
+          //   console.log(res.data);
+          // });
+      }, 
+      getElements: () => {
+        return getApiRoot()
+        .then((root) => {
+          return $http.get(`${root.elements}`);
+        }, e => console.error)
+        .then((res)=> {
+          return res.data;
+        }, e => console.error);
+      },
+      createElement: (data) => {
+        return getApiRoot()
+          .then((root) => {
+            return $http.post(`${root.elements}`, data);
+          }, e => console.error);
+          // .then((res) => {
+          //   console.log(res.data);
+          // });
       }
     };  
   }]);
