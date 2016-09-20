be1c0d2f81859b0ab9d406783487b621d7e7ed70
Megan Ford <meg.c.ford@gmail.com>
2016-09-20 16:04:16 -0500

    completed edit costume feature.

--- client/controllers/editCtrl.js
+++ client/controllers/editCtrl.js
@@ -4,9 +4,9 @@ app.controller("Edit",[
   "$timeout",
   "$location",
   "$uibModal",
-  "$rootScope", // need this because the  uib-Modal scope is not actually nested inside this scope- it's randomly off to one corner or whatever.
-  "costumeToEdit",  
-  function($scope, APIFactory, $timeout, $location, $uibModal, $rootScope, costumeToEdit) {
+  "$rootScope", // need this because the  uib-Modals' scope is not actually nested inside this scope- it's randomly off to one side or something.
+  "$routeParams", 
+  function($scope, APIFactory, $timeout, $location, $uibModal, $rootScope, $routeParams) {
     const edit = this;
 
     // Toggle 'edit tag' div.
@@ -14,6 +14,7 @@ app.controller("Edit",[
 
     edit.tags = [];
     edit.costumeelements = [];
+    //TODO: also get the costume's elements since they don't load as part of the costume. 
 
     edit.tag = {
       "name": "", 
@@ -21,132 +22,123 @@ app.controller("Edit",[
       "costumeelements": []
     };
 
-    create.costume = costumeToEdit;
-    console.log("costume to edit", costumeToEdit);
+    edit.costume = {};
 
-    // On load, get the username and user info. 
-    // $scope.$on("username", function(event, data) {
-    //   $timeout().then(() => {
-    //     create.username = data; 
-    //     return data;
-    //   }).then(() => {
-    //     return APIFactory.getUserInfo(create.username);
-    //   }).then((res) => {
-    //     create.userInfo = res; 
-    //     create.costume.owner = create.userInfo.url;
-    //     $timeout();
-    //   }, e=> console.error);
-    // });
+    //On load, set costume from route params.
+    APIFactory.getOneCostume($routeParams.id)
+    .then((res) => {
+      console.log("costume gotten", res);
+      edit.costume = res;
+      $timeout();
+    }, e => console.error)
+    .then(()=> {
+      return APIFactory.getCostumeElements(edit.costume.id);
+    }).then((res)=> {
+      edit.costumeelements = res;
+      $timeout();
+    }, e => console.error);
 
-    // // On load, also load all tags.
-    // APIFactory.getTags()
-    // .then((res)=> {
-    //   create.tags = res; 
-    //   $timeout();
-    // }, e => console.error);
-
-    // // In case of refresh. Gets all costume elements with no costume url assigned (meaning the costume has not been completed) and pushes their urls back into the costume object.
-    // APIFactory.getCostumeElements()
-    // .then((res) => {
-    //   create.costumeelements = res;
-    //   for(const index in create.costumeelements) {
-    //     create.costume.costumeelements.push(create.costumeelements[index].url);
-    //   }
-    //   $timeout();
-    // }, e => console.error);
-
-    // //grabs new data from the create modal.
-    // $rootScope.$on("createdSupply", function(event, value) { 
-    //   console.log("got the newly created supply", value);
-    //   create.costumeelements.push(value);
-    //   create.costume.costumeelements.push(value.url);
-    //   $timeout(); //Just in case.
-    // });
-
-    // //grabs updated data from edit modal.
-    // $rootScope.$on("editedSupply", function(event, value) { 
-    //   console.log("got the edited supply", value);
-    //   //remove old value.
-    //   for(const u in create.costumeelements) {
-    //     if(create.costumeelements[u].url === value.url) {
-    //       create.costumeelements.splice(u, 1);
-    //     }
-    //   } 
-    //   //replace with new value.
-    //   create.costumeelements.push(value);
-    //   $timeout(); //Just in case.
-    // });
-
-    // create.addTag = (url) => {
-    //   create.costume.tags.push(url);
-    // };
-
-    // create.removeTag = (url) => {
-    //   for(const u in create.costume.tags) {
-    //     if(create.costume.tags[u] === url) {
-    //       create.costume.tags.splice(u, 1);
-    //     }
-    //   }
-    // };
-
-    // create.createTag = () => {
-    //   APIFactory.createTag(create.tag).then((res) => {
-    //     create.tags.push(res);
-    //     create.costume.tags.push(res.url);
-    //     create.tagIsCollapsed = true;
-    //     create.tag.name="";
-    //     $timeout();
-    //   }, e => console.error);
-    // };
+    // On load, also load all tags.
+    APIFactory.getTags()
+    .then((res)=> {
+      edit.tags = res; 
+      $timeout();
+    }, e => console.error);
+    
 
 
-    // create.openCreateModal = () => {
-    //   const modalInstance = $uibModal.open({
-    //     size: "lg",
-    //     templateUrl: "/partials/createSupply.html", 
-    //     controller: "CreateSupply",
-    //     controllerAs: "createSupply"   
-    //   });
-    // }; 
+    //grabs new data from the create modal.
+    $rootScope.$on("createdSupply", function(event, value) { 
+      console.log("got the newly created supply", value);
+      edit.costumeelements.push(value);
+      edit.costume.costumeelements.push(value.url);
+      $timeout(); //Just in case.
+    });
+
+    //grabs updated data from edit modal.
+    $rootScope.$on("editedSupply", function(event, value) { 
+      console.log("got the edited supply", value);
+      //remove old value.
+      for(const u in edit.costumeelements) {
+        if(edit.costumeelements[u].url === value.url) {
+          edit.costumeelements.splice(u, 1);
+        }
+      } 
+      //replace with new value.
+      edit.costumeelements.push(value);
+      $timeout(); //Just in case.
+    });
+
+    edit.addTag = (url) => {
+      edit.costume.tags.push(url);
+    };
+
+    edit.removeTag = (url) => {
+      for(const u in edit.costume.tags) {
+        if(edit.costume.tags[u] === url) {
+          edit.costume.tags.splice(u, 1);
+        }
+      }
+    };
+
+    edit.createTag = () => {
+      APIFactory.createTag(edit.tag).then((res) => {
+        edit.tags.push(res);
+        edit.costume.tags.push(res.url);
+        edit.tagIsCollapsed = true;
+        edit.tag.name="";
+        $timeout();
+      }, e => console.error);
+    };
+  
+    edit.openCreateModal = () => {
+      const modalInstance = $uibModal.open({
+        size: "lg",
+        templateUrl: "/partials/editSupply.html", 
+        controller: "CreateSupply",
+        controllerAs: "editSupply"   
+      });
+    }; 
 
 
-    // create.openEditModal = (supply) => {
-    //   //Sending the entire object into modal.
-    //   const modalInstance = $uibModal.open({
-    //     size: "lg",
-    //     templateUrl: "/partials/editSupply.html", 
-    //     controller: "EditSupply",
-    //     controllerAs: "editSupply", 
-    //     resolve: {
-    //       "supply": supply
-    //     }
-    //   });
-    // };
+    edit.openEditModal = (supply) => {
+      //Sending the entire object into modal.
+      const modalInstance = $uibModal.open({
+        size: "lg",
+        templateUrl: "/partials/editSupply.html", 
+        controller: "EditSupply",
+        controllerAs: "editSupply", 
+        resolve: {
+          "supply": supply
+        }
+      });
+    };
 
-    // create.deleteSupply = (url) => {
-    //   APIFactory.deleteSomething(url)
-    //   .then(() => {
-    //     // remove from costume
-    //     for (const u in create.costume.costumeelements) {
-    //       if (create.costume.costumeelements[u] === url) {
-    //         create.costume.costumeelements.splice(u, 1);
-    //       }
-    //     }
-    //     // remove from costume element list
-    //     for (const u in create.costumeelements) {
-    //       if (create.costumeelements[u].url === url) {
-    //         create.costumeelements.splice(u, 1);
-    //       }
-    //     }
-    //     $timeout();
-    //   }, e => console.error);
-    // };
+    edit.deleteSupply = (url) => {
+      APIFactory.deleteSomething(url)
+      .then(() => {
+        // remove from costume
+        for (const u in edit.costume.costumeelements) {
+          if (edit.costume.costumeelements[u] === url) {
+            edit.costume.costumeelements.splice(u, 1);
+          }
+        }
+        // remove from costume element list
+        for (const u in edit.costumeelements) {
+          if (edit.costumeelements[u].url === url) {
+            edit.costumeelements.splice(u, 1);
+          }
+        }
+        $timeout();
+      }, e => console.error);
+    };
 
-    // create.createCostume = () => {
-    //   APIFactory.createCostume(create.costume).then((res)=> {
-    //     console.log(res);
-    //     // TODO: pop up with a success modal or something for a second. 
-    //     $location.path("/closet");
-    //   });
-    // };
+    edit.editCostume = () => {
+      APIFactory.editCostume(edit.costume.url, edit.costume)
+      .then(()=> {
+        console.log("yay, costume edited");
+        $location.path("/closet");
+      }, e => console.error);
+    };
+
   }]);
