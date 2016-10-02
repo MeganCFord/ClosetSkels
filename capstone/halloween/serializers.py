6e26c655af6aac98e2d17a541bb9dd341c7b0589
Megan Ford <meg.c.ford@gmail.com>
2016-10-02 14:01:44 -0500

    removed nope class, changed costumeelement to supply, did some cleaning up and commenting todos.

--- capstone/halloween/serializers.py
+++ capstone/halloween/serializers.py
@@ -5,34 +5,31 @@ from django.shortcuts import get_object_or_404
 import requests
 
 
-class NopeSerializer(serializers.HyperlinkedModelSerializer):
-  class Meta:
-    model = Nope
-    fields = ("nope")
-
 class ElementSerializer(serializers.HyperlinkedModelSerializer):
   class Meta: 
     model = Element
     fields = ("id", "url", "name")
 
+
 class TagSerializer(serializers.HyperlinkedModelSerializer):
    class Meta: 
     model = Tag
-    fields = ("id", "url", "name", "costumes", "costumeelements")
+    fields = ("id", "url", "name")
 
-class BooSerializer(serializers.HyperlinkedModelSerializer):
-  
+
+class BooSerializer(serializers.HyperlinkedModelSerializer): 
   class Meta: 
     model = Boo
     fields = ("url", "owner", "costume")
 
+
 class CostumeSerializer(serializers.HyperlinkedModelSerializer):
   boos=BooSerializer(many=True, read_only=True)
-  tags = TagSerializer(many=True)
+  tags = TagSerializer(many=True) 
 
   class Meta: 
     model = Costume
-    fields = ( 'id', 'url', 'owner', 'name', 'description', 'public', 'costumeelements', 'tags', 'boos', "image")
+    fields = ( 'id', 'url', 'owner', 'name', 'description', 'public', 'supplies', 'tags', 'boos', "image")
 
   def update(self, instance, validated_data):
     
@@ -42,12 +39,12 @@ class CostumeSerializer(serializers.HyperlinkedModelSerializer):
     instance.image = validated_data.get("image")
     instance.save()
     
-    ces_data = validated_data.pop("costumeelements", None)
+    ces_data = validated_data.pop("supplies", None)
     if ces_data: 
       instance.costumeeelements = [];
       for ce_data in ces_data:
         ce_id = getattr(ce_data, "id")
-        element_to_add = get_object_or_404(CostumeElement, pk=ce_id)
+        element_to_add = get_object_or_404(Supply, pk=ce_id)
         setattr(element_to_add, "costume", instance )
         element_to_add.save()
 
@@ -61,15 +58,16 @@ class CostumeSerializer(serializers.HyperlinkedModelSerializer):
 
     return instance
 
-class CostumeElementSerializer(serializers.HyperlinkedModelSerializer):
+
+class SupplySerializer(serializers.HyperlinkedModelSerializer):
   tags = TagSerializer(many=True)
   element = ElementSerializer()
   class Meta:
-    model = CostumeElement
+    model = Supply
     fields = ("id", "url", "element", "description", "tags", "name")
 
   def update(self, instance, validated_data):
-    # NOTE: not using this right now because the costumeelement itself is returning 404.
+    # NOTE: not using this right now because the supply itself is returning 404.
     instance.name = validated_data.get("name")
     instance.description = validated_data.get("description")
     
@@ -97,7 +95,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = User
     fields = ('id', 'url', 'username', 'costumes')
-
 
 
 
