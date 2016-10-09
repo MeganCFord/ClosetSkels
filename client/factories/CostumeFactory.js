985df8ecaf3dce90c570b4fef066a8fa440913d4
Megan Ford <meg.c.ford@gmail.com>
2016-10-09 13:59:23 -0500

    prelim supplieless create costume functionality added back.

--- client/factories/CostumeFactory.js
+++ client/factories/CostumeFactory.js
@@ -45,11 +45,24 @@ app.factory( "CostumeFactory", [
       }, 
 
       createCostume: (data) => {
-        console.log("data", data);
-      // return APIFactory.getApiRoot()
-      //   .then((root) => {
-      //     return $http.post(`${root.costumes}`, data);
-      //   }, errorHandle)
+        const supplies = data.supplies; 
+        console.log("costume supplies", supplies);
+        data.supplies = [];
+        return APIFactory.getApiRoot()
+        .then((root) => {
+          return $http.post(`${root.costumes}`, data);
+        }, errorHandle)
+        .then((res) => {
+          console.log("costume created", res.data);
+          // supplies.forEach((supply)=> {
+          // if the costume is being copied from an existing one, make new copies of the supplies, then update them with new costume instance. 
+          // if the costume is brand new, simply update them with the costume instance.
+          //   supply.costume = res.data.url;
+          //   return $http.put(`${supply.url}`, supply);
+          // }).then((res) => {
+          //   console.log("should have made a supply here", res.data);
+          // }, errorHandle);
+        });
       }
 
     };
