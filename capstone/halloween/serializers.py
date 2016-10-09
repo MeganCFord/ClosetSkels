38111a673cd634b5026e905c426a5f95e100ea76
Megan Ford <meg.c.ford@gmail.com>
2016-10-09 13:57:59 -0500

    moved create functionality into serializers. should work better than in views. these files are currently untested.

--- capstone/halloween/serializers.py
+++ capstone/halloween/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from halloween.models import *
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 import requests
 
 
@@ -27,43 +27,7 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
 class BooSerializer(serializers.HyperlinkedModelSerializer): 
   class Meta: 
     model = Boo
-    fields = ("url", "owner", "costume")
-
-
-class CostumeSerializer(serializers.HyperlinkedModelSerializer):
-  boos = BooSerializer(many=True, read_only=True)
-  tags = TagSerializer(many=True) 
-
-  class Meta: 
-    model = Costume
-    fields = ( 'id', 'url', 'owner', 'name', 'description', 'public', 'supplies', 'tags', 'boos', "image")
-
-  def update(self, instance, validated_data):
-    
-    instance.name = validated_data.get("name")
-    instance.description = validated_data.get("description")
-    instance.public = validated_data.get("public")
-    instance.image = validated_data.get("image")
-    instance.save()
-    
-    ces_data = validated_data.pop("supplies", None)
-    if ces_data: 
-      instance.costumeeelements = [];
-      for ce_data in ces_data:
-        ce_id = getattr(ce_data, "id")
-        element_to_add = get_object_or_404(Supply, pk=ce_id)
-        setattr(element_to_add, "costume", instance )
-        element_to_add.save()
-
-    tags_data = validated_data.pop("tags", None)
-    if tags_data: 
-      instance.tags = [];
-      for tag_data in tags_data:
-        tag_to_add = get_object_or_404(Tag, name=tag_data["name"])
-        instance.tags.add(tag_to_add)
-    instance.save()
-
-    return instance
+    fields = ("costume", "owner", "url")
 
 
 class SupplySerializer(serializers.HyperlinkedModelSerializer):
@@ -71,18 +35,47 @@ class SupplySerializer(serializers.HyperlinkedModelSerializer):
   element = ElementSerializer()
   class Meta:
     model = Supply
-    fields = ("id", "url", "element", "description", "tags", "name")
+    fields = ("id", "url", "element", "costume", "description", "tags", "name")
+
+  def create(self, request): 
+    '''
+    During creation of a supply,
+    adds instances of chosen tags to supply's tag list. 
+    '''
+    element = request.data["element"]
+    element_instance = Element.objects.get(id=element["id"])
+    new_supply = Supply(element = element_instance, name=request.data["name"], description=request.data["description"])
+
+    new_supply.save()
+
+    tags_data = request.data.pop("tags", None)
+    if tags_data:
+      for tag in request.data["tags"]:
+        tag_to_add = DjangoTag.objects.get(pk=tag["id"])
+        new_supply.tags.add(tag_to_add)
+      new_supply.save()
+    
+    # data = serializers.serialize("json", (new_supply,))
+    # return HttpResponse(data, status=status.HTTP_201_CREATED)
+    return new_supply
 
   def update(self, instance, validated_data):
-    # NOTE: not using this right now because the supply itself is returning 404.
+    '''
+    During updating of a supply:
+    1. Assign the costume instance (supplies are never created with costume field filled in),
+    2. Update the tags serialization list, 
+    3. Update the element instance.
+    '''
     instance.name = validated_data.get("name")
     instance.description = validated_data.get("description")
+    instance.save()
     
     element_data = validated_data.pop('element', None)
     if element_data:
-        element = get_object_or_404(Element, name=element_data["name"])
-        validated_data['element'] = element
-    instance.element = validated_data["element"]
+      element_to_add = Element.objects.get(pk=element_data["id"])
+      setattr(instance, "element", element_to_add)
+      instance.save()
+
 
     tags_data = validated_data.pop("tags", None)
     if tags_data: 
@@ -91,9 +84,86 @@ class SupplySerializer(serializers.HyperlinkedModelSerializer):
         tag_name = geattr(tag_data, "name")
         tag_to_add = get_object_or_404(Tag, name=tag_name)
         instance.tags.add(tag_to_add)
+      instance.save()
+
+    costume_data = validated_data.pop("costume", None)
+    if costume_data:
+      costume_to_add = Costume.objects.get(pk=costume_data["id"])
+      setattr(instance, "costume", costume_to_add)
+      instance.save()
+
+    return instance
+
+
+class CostumeSerializer(serializers.HyperlinkedModelSerializer):
+  boos = BooSerializer(many=True, read_only=True)
+  tags = TagSerializer(many=True) 
+  # TODO: add supply serialization into this. 
+
+  class Meta: 
+    model = Costume
+    fields = ( 'id', 'url', 'owner', 'name', 'description', 'public', 'supplies', 'tags', 'boos', "image")
+
+
+  def create(self, request): 
+    '''
+    Handles creation of tags and supplies during costume creation 
+    by adding newly created costume instance to existing tags and supplies. 
+    '''
+    owner = User.objects.get(id=request.data["owner"])
+    new_costume = Costume.objects.create(owner=owner, name=request.data["name"], description=request.data["description"], public=request.data["public"], image=request.data["image"])
+
+    new_costume.save()
+
+    tags_data = request.data.pop("tags", None)
+    if tags_data:
+      for tag in tags_data:
+        tag_to_add=Tag.objects.get(id=tag["id"])
+        new_costume.tags.add(tag_to_add)
+
+    new_costume.save()
+
+    # TEMP SOLUTION: handling supplies in front end.
+    # supply_data = request.data.pop("supplies", None)
+    # for supply in request.data["supplies"]:
+    #   supply_to_add = DjangoSupply.objects.get(id=supply["id"])
+    #   # TODO: what if it already is assigned to a costume? like via saving to closet.
+    #   setattr(supply_to_add, 'costume', new_costume)
+
+    # # Originally used this serialization pattern because JsonResponse doesn't seem to be able to serialize an instance.
+    # data = serializers.serialize("json", (new_costume,))
+    # return HttpResponse(data, status=status.HTTP_201_CREATED)  
+    return new_supply
+
+  def update(self, instance, validated_data):
+    
+    instance.name = validated_data.get("name")
+    instance.owner = validated_data.get("owner")
+    instance.description = validated_data.get("description")
+    instance.public = validated_data.get("public")
+    instance.image = validated_data.get("image")
+    instance.save()
+    
+    # TEMP SOLUTION: handling supplies on front end. 
+    # ces_data = validated_data.pop("supplies", None)
+    # if ces_data: 
+    #   for ce_data in ces_data:
+    #     ce_id = getattr(ce_data, "id")
+    #     element_to_add = Supply.objects.get(id = ce_id)
+    #     setattr(element_to_add, "costume", instance)
+    #     element_to_add.save()
+
+    tags_data = validated_data.pop("tags", None)
+    if tags_data: 
+      instance.tags = [];
+      for tag_data in tags_data:
+        tag_to_add = Tags.objects.get(pk=tag_data["id"])
+        instance.tags.add(tag_to_add)
     instance.save()
 
     return instance
+
+
 
 
 
--- capstone/halloween/views.py
+++ capstone/halloween/views.py
@@ -19,7 +19,6 @@ from halloween.serializers import *
 from django.core import serializers
 
 
-
 class User(viewsets.ModelViewSet):
   serializer_class = UserSerializer
   queryset = DjangoUser.objects.all()
@@ -37,11 +36,56 @@ class User(viewsets.ModelViewSet):
     return queryset
 
 
+class Element(viewsets.ModelViewSet):
+  queryset = DjangoElement.objects.all()
+  serializer_class = ElementSerializer
+
+
 class Tag(viewsets.ModelViewSet):
   queryset = DjangoTag.objects.all()
   serializer_class = TagSerializer
 
   # TODO: add a queryset here that allows me to just get the tags for a costume or supply.
+
+
+class Boo(viewsets.ModelViewSet):
+  queryset = DjangoBoo.objects.all()
+  serializer_class = BooSerializer
+
+  permission_classes = (IsOwnerOrReadOnly,)
+
+  def get_queryset(self): 
+    '''
+    Optionally restricts returned queryset to boos owned by a given user, 
+    by filtering against a 'userid' query perameter in the URL. 
+    '''
+    queryset = DjangoBoo.objects.all()
+    userid = self.request.query_params.get("userid", None)
+    if userid is not None:
+      queryset = queryset.filter(owner=userid)
+    return queryset
+
+
+class Supply(viewsets.ModelViewSet):
+  queryset = DjangoSupply.objects.all()
+  serializer_class = SupplySerializer
+
+  def get_queryset(self):
+    """
+    Optionally restricts the returned queryset to a given costume,
+    by filtering against a `costumeid` query parameter in the URL.
+    """
+    queryset = DjangoSupply.objects.all()
+    costumeid = self.request.query_params.get('costumeid', None)
+    if costumeid is not None:
+      queryset = queryset.filter(costume__id=costumeid)
+    else: 
+      key = self.request.query_params.get("key", None)
+      if key is not None:
+        queryset=queryset.filter(pk = key)
+      else:
+        queryset = queryset.filter(costume=None)
+    return queryset
 
 
 class Costume(viewsets.ModelViewSet):
@@ -72,99 +116,6 @@ class Costume(viewsets.ModelViewSet):
           queryset = queryset.filter(id=costumeid)
     return queryset
 
-  def create(self, request): 
-    '''
-    Handles creation of tags and supplies during costume creation: 
-    adds costume instance to existing tags and supplies. 
-    '''
-    existing_owner = DjangoUser.objects.get(id=request.data["owner"])
-    new_costume = DjangoCostume(owner = existing_owner, name=request.data["name"], description=request.data["description"], public=request.data["public"], image=request.data["image"])
-
-    new_costume.save()
-
-    for tag_id in request.data["tags"]:
-      tag_to_add = DjangoTag.objects.get(id=tag_id)
-      new_costume.tags.add(tag_to_add)
-
-    for ce_data in request.data["supplies"]:
-      # This doesn't seem to work. I don't want to use 'add' because it's not a many relationship. 
-      ce_to_add = DjangoSupply.objects.get(id=ce_data["id"])
-      setattr(ce_to_add, 'costume', new_costume)
-      new_costume.supplies.add(ce_to_add)
-
-    # Requires usage of this serialization pattern because JsonResponse isn't able to serialize an instance.
-    data = serializers.serialize("json", (new_costume,))
-    return HttpResponse(data, status=status.HTTP_201_CREATED)
-
-
-class Boo(viewsets.ModelViewSet):
-  queryset = DjangoBoo.objects.all()
-  serializer_class = BooSerializer
-
-  permission_classes = (IsOwnerOrReadOnly,)
-
-  def get_queryset(self): 
-    '''
-    Optionally restricts returned queryset to boos owned by a given user, 
-    by filtering against a 'userid' query perameter in the URL. 
-    '''
-    queryset = DjangoBoo.objects.all()
-    userid = self.request.query_params.get("userid", None)
-    if userid is not None:
-      queryset = queryset.filter(owner=userid)
-    return queryset
-
-class Element(viewsets.ModelViewSet):
-  queryset = DjangoElement.objects.all()
-  serializer_class = ElementSerializer
-
-
-class Supply(viewsets.ModelViewSet):
-  queryset = DjangoSupply.objects.all()
-  serializer_class = SupplySerializer
-
-  def get_queryset(self):
-    """
-    Optionally restricts the returned queryset to a given user,
-    by filtering against a `username` query parameter in the URL.
-    """
-    queryset = DjangoSupply.objects.all()
-    costume = self.request.query_params.get('costume', None)
-    if costume is not None:
-      queryset = queryset.filter(costume__id=costume)
-    else: 
-      key = self.request.query_params.get("key", None)
-      if key is not None:
-        queryset=queryset.filter(pk = key)
-      else:
-        queryset = queryset.filter(costume=None)
-    return queryset
-
-  def create(self, request): 
-    '''
-    Handles some sub-creation stuff during creation of an supply: 
-    if the request includes a costume instance, add it to the supply (in case I'm adding an element to an existing costume during costume edit), 
-    adds the supply instance to the list of supplies on tag. 
-    '''
-    existing_element = DjangoElement.objects.get(id=request.data["element"])
-    new_supply = DjangoSupply(element = existing_element, name=request.data["name"], description=request.data["description"])
-
-    new_supply.save()
-
-    try: 
-      # TODO: fix this, it's not working.
-      setattr(new_supply, "costume", request.data["costume"])
-    except: 
-      print("hey I couldn't add a costume to this supply")
-      pass
-
-    for tag_id in request.data["tags"]:
-      # TODO: I think I actually need to add the supply instance to the element...
-      tag_to_add = DjangoTag.objects.get(id=tag_id)
-      new_supply.tags.add(tag_to_add)
-    
-    data = serializers.serialize("json", (new_supply,))
-    return HttpResponse(data, status=status.HTTP_201_CREATED)
 
 # ###########
 # LOGIN STUFF 
@@ -200,9 +151,9 @@ def create_user(request):
   '''
   Receives request object from register form. Parses object by value (username, password, first_name, last_name), creates new user, saves to database
   Method arguments:
-  request = the full HTTP request object sent from register form
+  request = the full HTTP request object sent from register form.
   '''
-  # Load the JSON string of the request body into a dict
+  # Load the JSON string of the request body into a dict. Needs to be decoded bc of atob of password.
   req_body = json.loads(request.body.decode())
 
   UserName = req_body['username']
