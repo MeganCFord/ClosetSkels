28ff8903756d3cdb7fd2d7768bd8029775265b3f
Megan Ford <meg.c.ford@gmail.com>
2016-10-13 17:48:03 -0500

    created costume copy functionality.

--- client/factories/CostumeFactory.js
+++ client/factories/CostumeFactory.js
@@ -51,15 +51,15 @@ app.factory( "CostumeFactory", [
         }, errorHandle)
         .then((res) => {
           supplies.forEach((supply)=> {
-          // if the costume is brand new, simply update the supplies with the new costume instance on the costume field..
+          // if the costume is brand new, simply update the created null-costume supplies with the new costume instance on the costume field.
             supply.costume = String(res.data.url);
-            return $http.put(`${supply.url}`, supply)
+            return APIFactory.editSupply(supply)
             .then((res) => {
-            console.log("should have made a supply here", res.data);
+            console.log("should have made a supply here", res);
             }, errorHandle);
           });
         });
-      }
+      },
       // updateCostume: (data) => {
       //   return $http.put(`${data.url}`, data)
       //   .then((res) => {
@@ -67,7 +67,22 @@ app.factory( "CostumeFactory", [
       //   }, errorHandle);
       // }, 
       
-      // createCostumeCopy: 
+      copyCostume: (costume, supplies) => {
+        return APIFactory.getApiRoot()
+        .then((root)=> {
+          return $http.post(`${root.costumes}`, costume);
+        }, errorHandle)
+        .then((res)=> {
+          supplies.forEach((supply)=> {
+            // Since we're copying an existing costume, also copy all the existing supplies as new ones with the new costume URL.
+            supply.costume = String(res.data.url);
+            return APIFactory.createSupply(supply)
+            .then((res)=> {
+              console.log("should have made a supply copy here", res.data);
+            }, errorHandle);
+          });
+        });
+      }
 
     };
   }]);
